442a535742b5a1063c870aecbc434e490c8a11c0
Thuan Nguyen <thuannguyen2034@gmail.com>
2025-05-17 16:16:06 +0700

    API comment sẽ trả về commentResponseDto bọc trong responseObject

--- src/main/java/com/blog/conduit/controllers/CommentController.java
+++ src/main/java/com/blog/conduit/controllers/CommentController.java
@@ -3,8 +3,12 @@ package com.blog.conduit.controllers;
 import com.blog.conduit.dtos.CommentCreateRequestDto;
 import com.blog.conduit.dtos.CommentResponseDto;
 import com.blog.conduit.models.Comment;
+import com.blog.conduit.models.ResponseObject;
 import com.blog.conduit.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,13 +24,19 @@ public class CommentController {
     }
 
     @GetMapping
-    public List<CommentResponseDto> findByArticleSlug(@PathVariable("slug") String slug){
-        return commentService.findByArticleSlug(slug);
+    public ResponseEntity<ResponseObject> findByArticleSlug(@PathVariable("slug") String slug){
+        List<CommentResponseDto> commentResponseDtoList = commentService.findByArticleSlug(slug);
+        return commentResponseDtoList.isEmpty()?
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseObject("OK","no comment for this article with slug name: "+slug,"")):
+                ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("OK","found comments",commentResponseDtoList));
     }
 
     @PostMapping
-    public CommentResponseDto create(@PathVariable("slug") String slug, @RequestBody CommentCreateRequestDto commentCreateRequestDto){
+    public ResponseEntity<ResponseObject> create(@PathVariable("slug") String slug, @RequestBody CommentCreateRequestDto commentCreateRequestDto){
         commentCreateRequestDto.setSlug(slug);
-        return commentService.create(commentCreateRequestDto);
+        CommentResponseDto newCommentResponseDto = commentService.create(commentCreateRequestDto);
+        return newCommentResponseDto!=null?
+                ResponseEntity.status(HttpStatus.CREATED).body(new ResponseObject("OK","created comment",newCommentResponseDto)):
+                ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(new ResponseObject("failed","not created",""));
     }
 }
